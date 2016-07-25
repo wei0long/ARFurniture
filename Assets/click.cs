091eac4ff1a26e2106c41e085ef6b5630604fb6c
lyrican <294383855@qq.com>
2016-07-25 22:58:47 +0800

    changed the distibution of the furniture models and buttons.tested the app

---
+++ Assets/click.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click : MonoBehaviour {
+
+	// Use this for initialization
+	public void Onbt1Click ()
+	{
+		Debug.Log ("this is bt1");
+	}}
